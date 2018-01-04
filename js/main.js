d68550ecbf2ee68420a144198289fd0c7a5559e1
Tim A. Conner <timaconner1@gmail.com>
2018-01-04 10:57:12 -0600

    Added main js code.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@
 
 const $ = require("jquery");
 
+const userFactory = require("./userFactory");
+
+$("select").change(() => {
+    let selectedUser = $(this).val();
+    userFactory.getUsers(selectedUser);
+});
