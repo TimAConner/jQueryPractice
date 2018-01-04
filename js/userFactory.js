7de469f5985299ad77367474f51436ab0f18fd07
Tim A. Conner <timaconner1@gmail.com>
2018-01-04 10:54:48 -0600

    Add practice jquery.

---
+++ js/userFactory.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const $ = require("jquery");
+
+module.exports.getUsers = (selectedUser) => {
+    $.ajax({
+        url: "data/users.json"
+    })
+    .done((userData) => {
+        let usersArr = userData.users;
+        let selectedUserData = usersArr.filter((user) => {
+            return user.name === selectedUser;
+        })[0];
+    });
+};
